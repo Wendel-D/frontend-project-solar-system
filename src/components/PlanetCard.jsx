1a554ca417023eaf34d23b625846aefa3528fb95
Wendel Daniel <wendeldaniel263@gmail.com>
2022-08-18 15:46:55 -0300

    requisito 5 e 6 ok

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName } = this.props;
+    const { planetImage } = this.props;
+    const altText = `Planeta ${planetName}`;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{ planetName }</p>
+        <img src={ planetImage } alt={ altText } />
+      </div>
+    );
+  }
+}
+
+export default PlanetCard;
